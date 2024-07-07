9d784cb8a2acd187b3d3f188cbd05acacce53476
AnthonyAmaker <tamaker@gmail.com>
2024-07-07 19:35:36 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,11 @@
+var ghpages = require('gh-pages');
+require('dotenv').config();
+
+function finishedRunningCallback(){
+    console.log('done. Auto-generated commit ' + new Date().toLocaleString())
+}
+ghpages.publish(process.env.APP_PATH+'news-aggregate-file', {
+    branch: 'master',
+    repo: process.env.REPO_URL,
+    message: 'Auto-generated commit ' + new Date().toLocaleString()
+  }, finishedRunningCallback);
